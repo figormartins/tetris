21464fb9103def4ac050f38f4caffe3be86d041d
Igor Martins <figortmartins@gmail.com>
2020-06-19 18:31:43 -0300

    Now the Tetris are clearing rows

--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -4,8 +4,25 @@ import { createStage } from '../gameHelpers'
 
 export const useStage = (player, resetPlayer) => {
   const [stage, setStage] = useState(createStage())
+  const [rowsCleared, setRowsCleared] = useState(0)
 
   useEffect(() => {
+    setRowsCleared(0)
+
+    const sweepRows = newStage =>
+      newStage.reduce((ack, row) => {
+        if (row.findIndex(cell => cell[0] === 0) === -1) {
+          setRowsCleared(prev => prev + 1)
+          ack.unshift(new Array(newStage[0].length).fill([0, 'clear']))
+
+          return ack
+        }
+
+        ack.push(row)
+
+        return ack
+      }, [])
+
     const updateStage = prevStage => {
       const newStage = prevStage.map(row =>
         row.map(cell => (
@@ -24,8 +41,10 @@ export const useStage = (player, resetPlayer) => {
         })
       })
 
-      if (player.collided)
+      if (player.collided) {
         resetPlayer()
+        return sweepRows(newStage)
+      }
 
       return newStage
     }
@@ -33,5 +52,5 @@ export const useStage = (player, resetPlayer) => {
     setStage(prev => updateStage(prev))
   }, [player, resetPlayer])
 
-  return [stage, setStage]
+  return [stage, setStage, rowsCleared]
 }
