11072f048a5c37b5581260a732072e0fcf8f5623
Igor Martins <figortmartins@gmail.com>
2020-06-19 20:31:06 -0300

    Updates

---
+++ precache-manifest.574da2947b5655b985e559501e4d801d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da1c8848aea00d26a90ca0c03798eaae",
+    "url": "/tetris/index.html"
+  },
+  {
+    "revision": "14682e8dc929f2d761d0",
+    "url": "/tetris/static/js/2.16f3b69e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tetris/static/js/2.16f3b69e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83a3fa9df22524d493d2",
+    "url": "/tetris/static/js/main.a623a707.chunk.js"
+  },
+  {
+    "revision": "c5fd4f4c40a565bb27a8",
+    "url": "/tetris/static/js/runtime-main.8ec221a1.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/tetris/static/media/Pixel-LCD-7.e2897552.woff"
+  },
+  {
+    "revision": "2810fe6b5435e75b3dad63e341b97d02",
+    "url": "/tetris/static/media/bg.2810fe6b.png"
+  }
+]);
